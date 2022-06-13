7cc5b4595671d22de66298ccbd048bfac79eaac3
Hamza <alamhamza18@gmail.com>
2022-06-13 10:18:18 +0500

    FrontEnd

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: "auto",
+  });
+  const swiper_article = new Swiper('.swiper_article', {
+    direction: 'horizontal',
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  $("header.inbox_header .drop_down_icon").click(function(){
+    $(this).addClass("active")
+    $("header .drop_down_menu").addClass("show")
+  })
+  $(document).click(function(e) {
+    e.stopPropagation();
+    if (!$(event.target).closest('.drop_down_icon ').length && !$(event.target).closest('header .drop_down_menu').length) {
+        console.log("hello")
+        $("header .drop_down_menu").removeClass("show")
+        $("header.inbox_header .drop_down_icon").removeClass("active")
+    }
+});
+$(window).scroll(function() {    
+  var scroll = $(window).scrollTop();    
+  if (scroll >= 10) {
+      $("header").addClass("header_active");
+  } else{
+    $("header").removeClass("header_active");
+  }
+});
+$(".open_mobile_menu").click(function(){
+  if($("header .mobile_menu").css("left")=="-300px"){
+    $("header .mobile_menu").css("left","0px");
+    $("header").css("overflow","initial")
+    $("main").css("filter","blur(4px)")
+  } else{
+    $("header .mobile_menu").css("left","-300px");
+    $("header").css("overflow","hidden")
+    $("main").css("filter","blur(0px)")
+  }
+})
